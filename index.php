cd9756afd918da4a8c6c0293d69b6d07889d19ec
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-04 20:20:41 +0500

    add php echo code

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//***echo***//
+echo "PHP basics <br>";
+//for line break use <br>
+echo "hello world";
+echo"<br>";
+echo("learning php basics");
+//echo can work as a function as well
+
+?>
