b6176291f9851080754854abf0d3bbe266068339
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-04 20:41:45 +0500

    add variables notes

--- index.php
+++ index.php
@@ -7,4 +7,15 @@ echo"<br>";
 echo("learning php basics");
 //echo can work as a function as well
 
+
+//variables in php
+// just like in other languages, variables are also similar in php as well
+// but we don't have to declare the type of the variable that we do in js, we can directly declare them by using $ sign
+
+//ex
+echo "<br>Variables in PHP<br>";
+$name = "Carlos";
+echo $name;
+
+
 ?>
