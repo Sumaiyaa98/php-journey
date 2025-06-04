1f4d8c923d7ad3beca290964d6042193f47b131b
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-04 21:34:56 +0500

    add php basic notes

--- index.php
+++ index.php
@@ -19,3 +19,27 @@ echo $name;
 
 
 ?>
+
+<?php
+//we can also write html in php too
+// for that we just have to add the html tags inside the echo of php tags
+//ex
+//heading of html in php
+echo "<h1>Php with html</h1>";
+echo "<h2 style='color:purple'>Colorful text using html styling</h2>";
+
+
+//using html tags for php variables
+$myText = "hello world";
+echo "<h3>$myText</h3>";
+
+?>
+
+<h1>
+
+<!-- php tags inside html ones -->
+ <?php
+
+ echo "heyy"
+ ?>
+</h1>
