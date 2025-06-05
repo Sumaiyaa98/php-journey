fa664dc0d39398f8b647ef6c45042098069ba46a
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-05 14:23:17 +0500

    add datatypes notes

--- basics2.php
+++ basics2.php
@@ -28,4 +28,43 @@ echo var_dump(Name);
 
 //datatypes
 //similar datatypes,except one, name -> resource
+
+/**In PHP, the resource data type is a special type that holds a reference to an external resource, such as:
+
+        -a file handle (e.g., a file you're reading or writing to),
+
+        -a database connection (like MySQL),
+
+        -an image (when using image processing functions),
+
+        -or a stream.**/
+
+        
+        
+//🔹 Example of a Resource
+$file = fopen("example.txt", "r");  // Opens a file for reading
+var_dump($file);
+
+
+//🔸 Output:
+//resource(3) of type (stream)
+//This means $file is not a string or number — it's a resource, which points to an open file on your system.
+
+/**✅ When Do You Use It?
+Whenever you use PHP functions that interact with:
+
+Files: fopen(), fclose()
+
+Databases: mysqli_connect(), pdo
+
+Images: imagecreate(), imagepng()
+
+Streams / Network: fsockopen(), curl_init()
+
+You're usually working with a resource.**/
+
+
+//array datatype
+$arr = ["alex","john","carlos",20];
+echo var_dump($arr);
 ?>
